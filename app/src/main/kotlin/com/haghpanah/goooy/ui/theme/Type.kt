d79fc8a67359291aa125c578b2bed2c7bc841906
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2025-10-01 22:31:43 +0330

    set the new Font

--- app/src/main/kotlin/com/haghpanah/goooy/ui/theme/Type.kt
+++ app/src/main/kotlin/com/haghpanah/goooy/ui/theme/Type.kt
@@ -2,17 +2,101 @@ package com.haghpanah.goooy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.haghpanah.goooy.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+ val Typography = Typography(
+    displayLarge = Typography().displayLarge
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    displayMedium = Typography().displayMedium
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    displaySmall = Typography().displaySmall
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    headlineLarge = Typography().headlineLarge
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    headlineMedium = Typography().headlineMedium
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    headlineSmall = Typography().headlineSmall
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    titleLarge = Typography().titleLarge
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    titleMedium = Typography().titleMedium
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    titleSmall = Typography().titleSmall
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_bold))
+            )
+        ),
+    bodyMedium = Typography().bodyMedium
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        ),
+    bodyLarge = Typography().bodyLarge
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        ),
+    bodySmall = Typography().bodySmall
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        ),
+    labelLarge = Typography().labelLarge
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        ),
+    labelMedium = Typography().labelMedium
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        ),
+    labelSmall = Typography().labelSmall
+        .copy(
+            fontFamily = FontFamily(
+                fonts = listOf(Font(R.font.iran_sans_medium))
+            )
+        )
 )
